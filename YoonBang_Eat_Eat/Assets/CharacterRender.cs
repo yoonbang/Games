58ed521c500f01b032b6e2a4889e33cbacdd0ca6
yoonbang <successful1@naver.com>
2016-11-22 19:10:53 +0900

    캐릭터변경

---
+++ YoonBang_Eat_Eat/Assets/CharacterRender.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterRender : MonoBehaviour {
+    public Sprite Image;
+    public SpriteRenderer spriteRender;
+	// Use this for initialization
+	void Start () {
+        spriteRender = gameObject.GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ YoonBang_Eat_Eat/Assets/RenderImage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderImage : MonoBehaviour {
+    public Sprite Image;
+    public CharacterRender cr;
+    // Use this for initialization
+    void Start()
+    {
+        cr = GameObject.FindGameObjectWithTag("PlayerRender").GetComponent<CharacterRender>();
+    }
+    public void RenderChange()
+    {
+        cr.spriteRender.sprite = Image;
+    }
+}
